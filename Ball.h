c387635603190fd2a9f0ba49d03ca109afa4a399
nacerillo <nicholascerillo@gmail.com>
2015-03-18 09:12:48 -0400

    Create Ball.h

---
+++ Ball.h
@@ -0,0 +1,16 @@
+class Ball()
+
+int xPos;
+int yPos;
+xSpeed;
+ySpeed;
+const int widthBall;
+const int heightBall;
+
+public:
+  getX_Ball();
+  getY_Ball();
+  getBspeedX();
+  getBspeedY();
+  getWidthBall();
+  getHeightBall();
