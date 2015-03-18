7d4813c925cd7648e98e334163b415d77f15c31f
nacerillo <nicholascerillo@gmail.com>
2015-03-18 09:19:25 -0400

    Create Brick.h

---
+++ Brick.h
@@ -0,0 +1,19 @@
+class Brick{
+  int xPos;
+  int yPos;
+  const int widthBrick;
+  const int heightBrick;
+  bool Brickhit;
+  
+  public:
+    setXBrick();
+    setYBrick();
+    getWidthBrick();
+    setHeightBrick();
+
+    getXBrick();
+    getYBrick();
+    getWidthBrick();
+    getHeightBrick();
+    
+    };
