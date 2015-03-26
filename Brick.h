539d28dfbe8dd3cf9d4c373befba7d7de3ad0489
nacerillo <nicholascerillo@gmail.com>
2015-03-26 14:05:46 -0400

    Update Brick.h

--- Brick.h
+++ Brick.h
@@ -6,14 +6,15 @@ class Brick{
   bool Brickhit;
   
   public:
-    setXBrick(int X1);
-    setYBrick(int Y1);
-    getWidthBrick();
-    setHeightBrick();
-
-    getXBrick();
-    getYBrick();
-    getWidthBrick();
-    getHeightBrick();
+   void setXBrick(int X1);
+   void setYBrick(int Y1);
+   void setHeightBrick(int Hght);
+   void setWidthBrick(int W);
+   
+   int  getWidthBrick();
+   int  getXBrick();
+   int  getYBrick();
+   int getWidthBrick();
+   int getHeightBrick();
     
     };
