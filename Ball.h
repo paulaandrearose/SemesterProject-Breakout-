39dc114db77388227aed7443e27becef2fd5d5ef
nacerillo <nicholascerillo@gmail.com>
2015-04-07 14:20:34 -0400

    Update Ball.h

--- Ball.h
+++ Ball.h
@@ -1,17 +1,17 @@
 class Ball(){
 
-int xPos;
-int yPos;
-int xSpeed;
-int ySpeed;
-const int widthBall;
-const int heightBall;
+int xPos = 300;
+int yPos = 250;
+int xSpeed 2;
+int ySpeed 2;
+const int widthBall = 10;
+const int heightBall = 10;
 
 public:
-Ball(SDL_Renderer * renderer, int start_xspeed, int start_yspeed, int startX, int startY);
+Ball(SDL_Renderer * renderer, int xspeed, int yspeed, int startX, int startY);
 void move();
-void setBspeedX(int newSpeedx);
-void setBspeedY(int newSpeedy);
+void setBspeedX(int newXSpeed);
+void setBspeedY(int newYSpeed);
 void setWidthBall();
 void  setHeightBall();
 
@@ -21,6 +21,5 @@ int getBspeedX();
 int getBspeedY();
 int getWidthBall();
 int getHeightBall();
-  
   
 };
