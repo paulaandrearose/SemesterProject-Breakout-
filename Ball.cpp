82836dbe4268bf5af743de4fd8bbc593b71f64db
nacerillo <nicholascerillo@gmail.com>
2015-04-07 14:16:44 -0400

    Create Ball.cpp

---
+++ Ball.cpp
@@ -0,0 +1,47 @@
+#include <iostream>;
+#include "Ball.h";
+
+
+Ball::Ball(SDL_Renderer * rendererI, int start_xspeedI, int start_yspeedI, int startXI, int startYI)
+{
+ 
+}
+
+move::Ball()
+{
+  xPos += xSpeed;
+  yPos += ySpeed;
+
+ if(xPos >= 600){
+  xSpeed = -xSpeed;
+  }
+
+ else if(xPos <= 0){
+  xSpeed = -xSpeed;
+  }
+
+ else if(yPos >= 500){
+  ySpeed = -ySpeed;
+  }
+
+ else if(yPos <=0 ){
+  ySpeed = -ySpeed;
+  }
+
+}
+
+void setXspeed(int newSpeed){
+xSpeed = newSpeed;
+}
+
+void setYspeed(int newSpeed){
+  ySpeed = newSpeed;
+}
+
+int getX(){
+return xPos;
+}
+
+int getY(){
+return yPos;
+}
