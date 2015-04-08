bdbabe53d6678bb7d9a9a27ba91db418a1182b79
amandabaker <amandaabaker21@gmail.com>
2015-04-08 15:38:36 -0400

    Update Breakout.cpp

--- Breakout.cpp
+++ Breakout.cpp
@@ -7,40 +7,81 @@ using namespace std;
 const int SCREEN_WIDTH = 640;
 const int SCREEN_HEIGHT = 480;
 
-int main( int argc, char* args[]) {
-  //Window
-  SDL_Window* window = NULL;
+bool init();
+bool loadMedia();
+void close();
+SDL_Window* gWindow = NULL;
+SDL_Surface* gScreenSurface = NULL;
+
+bool init() {
+  //Initialization flag
+  bool success = true;
   
-  //Surface
-  SDL_Surface* screenSurface = NULL;
-  
-  //initialize SDL
-  if ( SDL_Init( SDL_INIT_VIDEO ) < 0 ) {
+  //Initialize SDL
+  if( SDL_Init( SDL_INIT_VIDEO ) < 0 ) {
     printf( "SDL could not initialize! SDL_Error: %s\n", SDL_GetError() );
+    success = false;
   }
   else {
     //Create Window
-    window = SDL_CreateWindow ( "Breakout!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
-    if ( window == NULL ) {
-      printf ( "Window could not be created! SDL_Error: %s\n", SDL_GetError();
+    gWindow = SDL_CreateWindow( "Breakout!", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN );
+    if( gWindow == NULL ) {
+      printf( "Window could not be created! SDL_Error: %s\n", SDL_GetError() );
+      success = false;
+    }
+    else {
+      //Get window surface
+      gScreenSurface = SDL_GetWindowSurface( gWindow );
+    }
+  }
+  
+  return success;
+}
+
+bool loadMedia() {
+  //Loading success flag
+  bool success = true;
+  
+  //TODO here is where we would load a background if necessary. 
+}
+
+void close() {
+  //Deallocate surface
+  SDL_FreeSurface( /*Surface name*/ );
+  /*Surface name*/ = NULL;
+  
+  //Destory Window
+  SDL_DestroyWindow( gWindow );
+  gWindow = NULL;
+  
+  //Quit SDL subsystems
+  SDL_Quit();
+}
+
+int main( int argc, char* args[]) {
+  
+  //initialize SDL and create window
+  if ( !init() ) {
+    printf( "Failed to initialize!\n" );
+  }
+  else {
+    //Create Window
+    if ( !loadMedia() ) {
+      printf ( "Failed to load media!\n" );
     }
     else {
       //Get window surface
       screenSurface = SDL_GetWindowSurface ( window );
       
-      //Fill surface white
-      SDL_FillRect ( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0xFF, 0xFF, 0xFF ) );\
+      //Fill surface black
+      SDL_FillRect ( screenSurface, NULL, SDL_MapRGB( screenSurface->format, 0x00, 0x00, 0x00 ) );\
       
       //Update the surface
-      SDL_UpdateWindowSurface( window );
+      SDL_UpdateWindowSurface( gWindow );
     }
   }
   
-  //Destroy window
-  SDL_DestroyWindow( window );
-  
-  //Quit SDL subsystems
-  SDL_Quit();
+  close();
   
   return 0;
 }
