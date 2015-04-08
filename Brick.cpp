cf2576e391fb9832efe5603f871dc1667e069c02
nacerillo <nicholascerillo@gmail.com>
2015-04-08 13:40:52 -0400

    Create Brick.cpp

---
+++ Brick.cpp
@@ -0,0 +1,14 @@
+#incluce "Brick.h"
+
+Brick::Brick(int height, int width, int xPos, int yPos)
+{
+height = Brickhieght;
+width = Brickwidth;
+x = xPos;
+y = yPos;
+}
+
+getX::getX()
+{
+return xPos;
+}
