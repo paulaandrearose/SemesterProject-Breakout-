5e627ce0cb6303511eeeb77bbfb6594166fc483d
amandabaker <amandaabaker21@gmail.com>
2015-04-09 12:49:11 -0400

    Update Paddle.cpp
    Note: main function can be removed, but is being commented out just in case we needed it...

--- Paddle.cpp
+++ Paddle.cpp
@@ -1,46 +1,63 @@
 #include "Paddle.h"
 using namespace std;
 
-const int SCREEN_WIDTH = 640;
-SDL_Renderer* gRenderer = NULL;
-const int SCREEN_HEIGHT = 480;
 int leftBoundry = 0;
 int rightBoundry = SCREEN_WIDTH;
 
 Paddle::Paddle(){
 	int height = 20;
 	int width = 100;
-	int xPos = SCREEN_WIDTH/2;
-	int yPos = SCREEN_HEIGHT - SCREEN_HEIGHT/12;
-	int speed = 0;
+	int xPos = SCREEN_WIDTH/2 - width/2;
+	int yPos = 400  /*SCREEN_HEIGHT - SCREEN_HEIGHT/12*/;
+	int xVel = 0;
 }
 
 void Paddle::movePaddle(){
-	xPos += speed;
+	//xPos += xVel * velMult;
+	xPos += xVel;
 
 	if(xPos <= leftBoundry)
 	{
 		xPos = leftBoundry;
 	}
 
-	if(xPos >= rightBoundry)
+	if(xPos >= rightBoundry - width)
 	{
-		xPos = rightBoundry;
+		xPos = rightBoundry - width;
 	}
 }
 
-void Paddle::setSpeed(int newSpeed){
-	speed = newSpeed;
+void Paddle::setXVel(int newVel){
+	xVel = newVel;
 }
 
-int Paddle::getSpeed(){
-	return speed;
-}
+int Paddle::getXVel(){
+	return xVel;
+} 
 
 int Paddle::getX(){
 	return xPos;
 }
 
+int Paddle::getY() {
+	return yPos;
+}
+
+int Paddle::getWidth() {
+	return width;
+}
+
+int Paddle::getHeight() {
+	return height;
+}
+
+void Paddle::render( SDL_Renderer* gRenderer ) {
+	SDL_Rect fillRect = { xPos, yPos, width, height };
+	SDL_SetRenderDrawColor( gRenderer, 0x50, 0x90, 0xFF, 0xFF );
+	SDL_RenderFillRect( gRenderer, &fillRect );
+}
+
+/*
 int main(){
 
 	Paddle pad();
@@ -88,3 +105,4 @@ int main(){
 
 	return 0;
 }
+*/
