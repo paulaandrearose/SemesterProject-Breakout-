84174c2b55def402a4ac240a43c3ceceba09d21e
amandabaker <amandaabaker21@gmail.com>
2015-04-09 20:47:50 -0400

    Create Brick.cpp

--- Brick.cpp
+++ Brick.cpp
@@ -1,14 +1,30 @@
-#incluce "Brick.h"
+#include "Brick.h"
+using namespace std;
 
-Brick::Brick(int height, int width, int xPos, int yPos)
-{
-height = Brickhieght;
-width = Brickwidth;
-x = xPos;
-y = yPos;
+
+Brick::Brick() {
+	int xPos = 0;
+	int yPos = 0;
+	int width = 0;
+	int height = 0;
 }
 
-getX::getX()
-{
-return xPos;
+Brick::Brick( int x, int y, int w, int h ) {
+	int xPos = x;
+	int yPos = y;
+	int width = w;
+	int height = h;
+}
+
+void Brick::set( int x, int y, int w, int h ) {
+	xPos = x;
+	yPos = y;
+	width = w;
+	height = h;
+}
+
+void Brick::render(SDL_Renderer* gRenderer) {
+	SDL_Rect fillRect = { xPos, yPos, width, height };
+	SDL_SetRenderDrawColor( gRenderer, 0xFF, 0xFF, 0xFF, 0xFF );
+	SDL_RenderFillRect( gRenderer, &fillRect );
 }
